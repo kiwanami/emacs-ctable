2856052011c85fb01b416056c4b98b3624a9aadc
SAKURAI Masashi <m.sakurai@kiwanami.net>
2013-06-10 09:51:11 +0900

    Moved structure documents into the defstruct slot.

--- ctable.el
+++ ctable.el
@@ -74,17 +74,22 @@ click-hooks : a list of functions for header clicking with two arguments
 ;;
 
 (defstruct ctbl:param
-  display-header ;; if t, display the header row with column models.
-  fixed-header   ;; if t, display the header row in the header-line area.
-  bg-colors      ;; '(((row-id . col-id) . colorstr) (t . default-color) ... ) or (lambda (model row-id col-id) colorstr or nil)
-  vline-colors   ;; "#RRGGBB" or '((0 . colorstr) (t . default-color)) or (lambda (model col-index) colorstr or nil)
-  hline-colors   ;; "#RRGGBB" or '((0 . colorstr) (t . default-color)) or (lambda (model row-index) colorstr or nil)
-  draw-vlines    ;; 'all or '(0 1 2 .. -1) or (lambda (model col-index) t or nil )
-  draw-hlines    ;; 'all or '(0 1 2 .. -1) or (lambda (model row-index) t or nil )
-  vertical-line horizontal-line ;; | -
-  left-top-corner right-top-corner left-bottom-corner right-bottom-corner  ;; +
-  top-junction bottom-junction left-junction right-junction cross-junction ;; +
-  )
+"Rendering parameters
+
+display-header : if t, display the header row with column models.
+fixed-header   : if t, display the header row in the header-line area.
+bg-colors      : '(((row-id . col-id) . colorstr) (t . default-color) ... ) or (lambda (model row-id col-id) colorstr or nil)
+vline-colors   : \"#RRGGBB\" or '((0 . colorstr) (t . default-color)) or (lambda (model col-index) colorstr or nil)
+hline-colors   : \"#RRGGBB\" or '((0 . colorstr) (t . default-color)) or (lambda (model row-index) colorstr or nil)
+draw-vlines    : 'all or '(0 1 2 .. -1) or (lambda (model col-index) t or nil )
+draw-hlines    : 'all or '(0 1 2 .. -1) or (lambda (model row-index) t or nil )
+vertical-line horizontal-line : | -
+left-top-corner right-top-corner left-bottom-corner right-bottom-corner  : +
+top-junction bottom-junction left-junction right-junction cross-junction : +"
+  display-header fixed-header
+  bg-colors vline-colors hline-colors draw-vlines draw-hlines vertical-line horizontal-line
+  left-top-corner right-top-corner left-bottom-corner right-bottom-corner
+  top-junction bottom-junction left-junction right-junction cross-junction)
 
 (defvar ctbl:default-rendering-param
   (make-ctbl:param
